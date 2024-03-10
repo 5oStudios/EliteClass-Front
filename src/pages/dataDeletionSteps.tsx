c33c22b9110978c630ca6e9a2236bb6408527f88
amirSaid <amir@enegix.com>
2024-03-10 13:44:40 +0200

    deletePage

---
+++ src/pages/dataDeletionSteps.tsx
@@ -0,0 +1,21 @@
+const deleteSteps = ()=>{
+    return (
+    <>
+        <h1>Elite Class Account Deletion</h1>
+        <ol>
+            <li><strong>Open Elite Class App:</strong> Launch the Elite Class mobile app on your device.</li>
+            <li><strong>Sign In:</strong> If not signed in, enter your credentials to log in to your account.</li>
+            <li><strong>Navigate to Profile:</strong> Tap on the profile icon or menu option.</li>
+            <li><strong>Access Account Settings:</strong> Find and tap on "Account Settings" or a similar option.</li>
+            <li><strong>Delete Account:</strong> Look for the "Delete Account" option.</li>
+            <li><strong>Confirm Deletion:</strong> Confirm your decision to delete your account.</li>
+            <li><strong>Provide Additional Information (if required):</strong> Fill in any required details.</li>
+            <li><strong>Review Terms and Conditions (if required):</strong> Read and review any terms related to account deletion.</li>
+            <li><strong>Final Confirmation:</strong> Confirm the account deletion.</li>
+            <li><strong>Notification:</strong> Receive a confirmation message for successful account deletion.</li>
+        </ol>
+        <p>Remember to be cautious, as account deletion is usually irreversible. Review the app's terms and conditions for more information.</p>
+    </>
+    );
+};
+export default deleteSteps
